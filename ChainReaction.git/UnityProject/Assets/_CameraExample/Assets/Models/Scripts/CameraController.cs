aae570a9805f5106a52053cbc23e9925c3dc7b9d
Headgate <paulwmattson@gmail.com>
2016-02-15 16:20:15 -0700

    Example Camera Project
    This is a camera placeholder test.

---
+++ ChainReaction.git/UnityProject/Assets/_CameraExample/Assets/Models/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	
+	public GameObject Camera1;
+	public GameObject Camera2;
+	
+	private bool mMainCamera;
+
+	private float mDragThreshold = 10;
+	private Vector3 mMouseDownPosition;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Camera1.SetActive(false);
+		mMainCamera = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown() {
+		mMouseDownPosition = Input.mousePosition;
+	}
+	
+	void OnMouseUp() {
+		Vector3 moved = (Input.mousePosition - mMouseDownPosition);
+		if (moved.magnitude < mDragThreshold) 
+		{
+			if (mMainCamera) 
+			{
+				Camera1.SetActive(false);
+				Camera2.SetActive(true);
+				mMainCamera = false;
+			}
+			else 
+			{
+				Camera1.SetActive(true);
+				Camera2.SetActive(false);
+				mMainCamera = true;
+			}
+		}
+	}
+}
